e7fed6c02a4902441c90c7208304ec85a7fbc8ea
Bordes Remy <remy.bordes@ynov.com>
2025-11-19 14:53:42 +0100

    feat : adding events for tama

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from src.tamagotchi import Tamagotchi
+from src.events import *
 
 if __name__ == '__main__':
     t = Tamagotchi("BOB")
     print(t)
+
+    Feed(t)
+    print(t)
+    Sleep(t)
+    print(t)
+    Play(t)
+    print(t)
+    Wash(t)
+    print(t)
+
